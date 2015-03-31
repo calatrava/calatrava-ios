ed077e699dcd0a596a734e1683dea99271625f7d
Tapan Thaker <tapan.d.thaker@gmail.com>
2015-03-31 11:37:41 +0530

    Added BridgeJSProtocol for JSExport

---
+++ calatrava-ios/Bridge/BridgeJSProtocol.h
@@ -0,0 +1,22 @@
+
+#import <JavaScriptCore/JavaScriptCore.h>
+
+@protocol BridgeJSProtocol <JSExport>
+
+- (void)changeToPage:(NSString *)target;
+- (void)registerProxy:(NSString *)proxy forPage:(NSString *)name;
+- (void)attachHandlerTo:(NSString *)proxy forEvent:(NSString *)name;
+- (void)valueFrom:(NSString *)proxy forField:(NSString *)field returnedTo:(NSString *)getId;
+- (void)render:(NSString *)proxy with:(NSDictionary *)dataMsg;
+- (void)requestFrom:(NSString *)reqId
+              url:(NSString *)url
+               as:(NSString *)method
+             with:(NSString *)body
+          headers:(NSDictionary *)headers;
+- (void)openUrl:(NSString *)url;
+- (void)startTimer:(NSString *)timerId timeout:(int)timeout;
+- (void)callPlugin:(NSString *)plugin
+          method:(NSString *)method
+        withArgs:(NSDictionary *)args;
+
+@end
