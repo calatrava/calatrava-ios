6d23a34216d2a08fa963aaa8efac16d188b736aa
Tapan Thaker <tapan.d.thaker@gmail.com>
2015-03-31 11:42:25 +0530

    Implementing the BridgeJSProtocol methods in JSCoreRuntime

--- calatrava-ios/Bridge/JSCoreRuntime.m
+++ calatrava-ios/Bridge/JSCoreRuntime.m
@@ -97,5 +97,39 @@
     }
 }
 
+- (void)changeToPage:(NSString *)target{
+    [self.pageDelegate changeToPage:target];
+}
+- (void)registerProxy:(NSString *)proxy forPage:(NSString *)name{
+    [self.pageDelegate registerProxy:proxy forPage:name];
+}
+- (void)attachHandlerTo:(NSString *)proxy forEvent:(NSString *)name{
+    [self.pageDelegate attachHandlerTo:proxy forEvent:name];
+}
+- (void)valueFrom:(NSString *)proxy forField:(NSString *)field returnedTo:(NSString *)getId{
+    [self.pageDelegate valueFrom:proxy forField:field returnedTo:getId];
+}
+- (void)render:(NSString *)proxy with:(NSDictionary *)dataMsg{
+    [self.pageDelegate render:proxy with:dataMsg];
+}
+- (void)requestFrom:(NSString *)reqId
+                url:(NSString *)url
+                 as:(NSString *)method
+               with:(NSString *)body
+            headers:(NSDictionary *)headers{
+    [self.requestDelegate requestFrom:reqId url:url as:method with:body headers:headers];
+}
+- (void)openUrl:(NSString *)url{
+    [self.uiDelegate openUrl:url];
+}
+- (void)startTimer:(NSString *)timerId timeout:(int)timeout{
+    [self.timerDelegate startTimer:timerId timeout:timeout];
+}
+- (void)callPlugin:(NSString *)plugin
+            method:(NSString *)method
+          withArgs:(NSDictionary *)args{
+    [self.pluginDelegate callPlugin:plugin method:method withArgs:args];
+}
+
 @end
 
